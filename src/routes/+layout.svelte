211e682502c7162b92200121871d1286b85d8231
Barbora Kovalská <barca.kovalska@gmail.com>
2025-11-13 21:56:54 +0100

    colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
+
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
 
 <nav class="flex gap-4 bg-gray-800 p-4 text-white">
 	<a
@@ -14,6 +18,11 @@
 	<a
 		href="/sorting"
 		class="hover:underline">Sorting</a>
+	<button
+		class="ml-auto rounded bg-gray-600 px-4 py-2 hover:bg-gray-500"
+		onclick={toggleMode}>
+		Toggle Mode
+	</button>
 </nav>
 
 <main class="p-8">
