15606b7a72e179fcd02bf723868e32fac438c478
Kkobari <barca.kovalska@gmail.com>
2026-03-13 18:15:08 +0100

    fixed build error

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -13,42 +13,42 @@
 		<ul class="space-y-2">
 			<li>
 				<a
-					href="data-structures/binary-search-tree"
+					href="/{locale}/data-structures/binary-search-tree"
 					class="text-primary hover:underline">{t('dataStructures.binarySearchTree')}</a>
 			</li>
 			<li>
 				<a
-					href="data-structures/avl-tree"
+					href="/{locale}/data-structures/avl-tree"
 					class="text-primary hover:underline">{t('dataStructures.avlTree')}</a>
 			</li>
 			<li>
 				<a
-					href="data-structures/red-black-tree"
+					href="/{locale}/data-structures/red-black-tree"
 					class="text-primary hover:underline">{t('dataStructures.redBlackTree')}</a>
 			</li>
 			<li>
 				<a
-					href="data-structures/b-tree"
+					href="/{locale}/data-structures/b-tree"
 					class="text-primary hover:underline">{t('dataStructures.bTree')}</a>
 			</li>
 			<li>
 				<a
-					href="data-structures/heap"
+					href="/{locale}/data-structures/heap"
 					class="text-primary hover:underline">{t('dataStructures.heap')}</a>
 			</li>
 			<li>
 				<a
-					href="data-structures/linked-list"
+					href="/{locale}/data-structures/linked-list"
 					class="text-primary hover:underline">{t('dataStructures.linkedList')}</a>
 			</li>
 			<li>
 				<a
-					href="data-structures/stack"
+					href="/{locale}/data-structures/stack"
 					class="text-primary hover:underline">{t('dataStructures.stack')}</a>
 			</li>
 			<li>
 				<a
-					href="data-structures/queue"
+					href="/{locale}/data-structures/queue"
 					class="text-primary hover:underline">{t('dataStructures.queue')}</a>
 			</li>
 		</ul>
@@ -59,32 +59,32 @@
 		<ul class="space-y-2">
 			<li>
 				<a
-					href="sorting-algorithms/bubble"
+					href="/{locale}/sorting-algorithms/bubble"
 					class="text-primary hover:underline">{t('sortingAlgorithms.bubbleSort')}</a>
 			</li>
 			<li>
 				<a
-					href="sorting-algorithms/selection"
+					href="/{locale}/sorting-algorithms/selection"
 					class="text-primary hover:underline">{t('sortingAlgorithms.selectionSort')}</a>
 			</li>
 			<li>
 				<a
-					href="sorting-algorithms/insertion"
+					href="/{locale}/sorting-algorithms/insertion"
 					class="text-primary hover:underline">{t('sortingAlgorithms.insertionSort')}</a>
 			</li>
 			<li>
 				<a
-					href="sorting-algorithms/merge"
+					href="/{locale}/sorting-algorithms/merge"
 					class="text-primary hover:underline">{t('sortingAlgorithms.mergeSort')}</a>
 			</li>
 			<li>
 				<a
-					href="sorting-algorithms/quick"
+					href="/{locale}/sorting-algorithms/quick"
 					class="text-primary hover:underline">{t('sortingAlgorithms.quickSort')}</a>
 			</li>
 			<li>
 				<a
-					href="sorting-algorithms/heap"
+					href="/{locale}/sorting-algorithms/heap"
 					class="text-primary hover:underline">{t('sortingAlgorithms.heapSort')}</a>
 			</li>
 		</ul>
